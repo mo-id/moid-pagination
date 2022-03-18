8b9cc5c5ad25d974ff6fe1fe6f7bb378731a4bf4
Federico Baldini <federicobaldini@192.168.1.57>
2022-03-18 16:38:17 +0100

    update post build

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -6,8 +6,6 @@ mkdir ./react
 mkdir ./types
 
 echo 'export * from "../__dist/esm/index3";' > ./react/index.js
-echo 'export * from "../__dist/types/Paginated.interface.d";' > ./types/index.js
+echo 'export * from "./index.d";' > ./types/index.js
 
 echo 'export * from "../__dist/react";' > ./react/index.d.ts
-echo 'export * from "../__dist/types/Paginated.interface.d";' > ./types/index.d.ts
-# echo 'export * from "../__dist/types/Pagination.interface.d";' > ./types/index.d.ts
